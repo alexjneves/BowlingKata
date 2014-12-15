eba9401089f396f317f9736886503e3151fbc9f9
alexjneves <alexneves08@gmail.com>
2014-12-15 13:19:31 +0000

    Before Last Frame Case
    Completed up to the case of the final frame, where additional rolls are
    required if a spare of strike is rolled

---
+++ BowlingKata.Test/BowlingKataTests.cs
@@ -0,0 +1,73 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace BowlingKata.Test
+{
+    [TestFixture]
+    public class BowlingKataTests
+    {
+        private Bowling _game;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _game = new Bowling();
+        }
+
+        [Test]
+        public void WhenRollingAllZeroes_ScoreShouldBeZero()
+        {
+            RollMany(0, 20);
+
+            _game.CalculateScore().Should().Be(0);
+        }
+
+        [Test]
+        public void WhenRollingAllOnes_ScoreShouldBeTwenty()
+        {
+            RollMany(1, 20);
+
+            _game.CalculateScore().Should().Be(20);
+        }
+
+        [Test]
+        public void WhenRollingASpare_ShouldCalculateCorrectScore()
+        {
+            _game.Roll(6);
+            _game.Roll(4);
+            _game.Roll(3);
+
+            RollMany(0, 17);
+
+            _game.CalculateScore().Should().Be(16);
+        }
+
+        [Test]
+        public void WhenRollingAStrike_ShouldCalculateCorrectScore()
+        {
+            _game.Roll(10);
+
+            _game.Roll(6);
+            _game.Roll(2);
+
+            _game.CalculateScore().Should().Be(26);
+        }
+
+        [Test]
+        public void WhenRollingAllStrikes_ShouldCalculateCorrectScore()
+        {
+            RollMany(10, 12);
+
+            _game.CalculateScore().Should().Be(300);
+        }
+
+        private void RollMany(int pins, int n)
+        {
+            for (var i = 0; i < n; ++i)
+            {
+                _game.Roll(pins);
+            }
+        }
+
+    }
+}
---
+++ BowlingKata/Exceptions/BowlingException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BowlingKata.Exceptions
+{
+    public class BowlingException : Exception
+    {
+        public BowlingException()
+        {
+        }
+
+        public BowlingException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public BowlingException(string message) : base(message)
+        {
+        }
+    }
+
+    public class FrameCompleteException : BowlingException
+    {
+        public FrameCompleteException()
+        {
+        }
+
+        public FrameCompleteException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public FrameCompleteException(string message) : base(message)
+        {
+        }
+    }
+}
